c00899bd66c2902dd8c14428a933ccfadd790ef1
cemno <82771105+cemno@users.noreply.github.com>
2021-11-24 22:55:19 +0100

    cloud changes

--- exercise_9.R
+++ exercise_9.R
@@ -2,10 +2,10 @@ library(chillR)
 
 station_list <- handle_gsod(action = "list_stations",
                             location = c(y = 61.623929, x = 12.677148),
-                            time_interval = c(1980, 2020))
+                            time_interval = c(1999, 2020))
 weather <- handle_gsod(action = "download_weather",
                        location = "023070_99999",
-                       time_interval = c(1980,2020))
+                       time_interval = c(1999,2020))
 weather_cleaned <- handle_gsod(weather)
 weather_cleaned <- weather_cleaned$weather
 
