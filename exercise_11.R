5baf34dac3059aa5893a402b648bda23df6c19a4
cemno <82771105+cemno@users.noreply.github.com>
2022-01-10 10:26:28 +0100

    cloud changes

--- exercise_11.R
+++ exercise_11.R
@@ -1,5 +1,6 @@
 # Weather Generator
 library(chillR)
+library(tidyverse)
 library(ggplot2)
 
 fulufjaellet_weather <- read.csv("data/fulufjaellet_weather.csv")
@@ -28,3 +29,74 @@ ggplot(data=temperature_compare, aes(date,Tmin)) +
   theme(legend.position = "none") +
   scale_x_date(date_labels = "%b")
 
+ggplot(data=temperature_compare, aes(date,Tmax)) +
+  geom_smooth(aes(colour = factor(Year))) +
+  facet_wrap(vars(data_source)) +
+  theme_bw(base_size = 20) +
+  theme(legend.position = "none") +
+  scale_x_date(date_labels = "%b")
+
+# Chill distribution
+
+# As the location of interest is rather close to the polar circle, a usual growing season (or dormancy period) might not be applicable.
+# Thus the time frame relevant for chill portions accumulation should be further investigated.
+# Different season lengths were tested, centering around January 13, as the hottest mean temperature was around July 13.
+
+chill_observed <- chilling(stack_hourly_temps(weather = temperature_compare[which(temperature_compare$data_source=="observed"),],
+                                              latitude = 61.6),
+                           Start_JDay = 196,
+                           End_JDay = 195)
+chill_simulated <- chilling(stack_hourly_temps(weather = temperature_compare[which(temperature_compare$data_source=="simulated"),],
+                                              latitude = 61.6),
+                            Start_JDay = 196,
+                            End_JDay = 195)
+
+chill_comparison <- cbind(chill_observed, data_source = "observed")
+chill_comparison <- rbind(chill_comparison, cbind(chill_simulated, data_source = "simulated"))
+
+chill_comparison_max_season <- chill_comparison[which(chill_comparison$Perc_complete == 100),]
+
+for (i in c(182:50)){
+  chill_observed <- chilling(stack_hourly_temps(weather = temperature_compare[which(temperature_compare$data_source=="observed"),],
+                                                latitude = 61.6),
+                             Start_JDay = 365-i+13,
+                             End_JDay = i-1)
+  chill_simulated <- chilling(stack_hourly_temps(weather = temperature_compare[which(temperature_compare$data_source=="simulated"),],
+                                                 latitude = 61.6),
+                              Start_JDay = 365-i+13,
+                              End_JDay = i-1)
+  chill_comparison <- rbind(chill_comparison, cbind(chill_observed, data_source = "observed"))
+  chill_comparison <- rbind(chill_comparison, cbind(chill_simulated, data_source = "simulated"))
+}
+
+chill_comparison_full_seasons <- chill_comparison[which(chill_comparison$Perc_complete == 100),]
+
+# As to be seen, the Chill portions are highly dependent on the considered days to make up one season. This suggests even in summer its cold enough to accumulate chill portions.
+# The rate for Chill portion accumulation seem to decrease only slightly in summer. Thus the whole season is considered relevant int he following study of this specific location.
+ggplot(chill_comparison_full_seasons, aes(x = as.factor(Season_days)))+
+  geom_boxplot(aes(y = Chill_portions, colour = factor(data_source)))+
+  scale_x_discrete(breaks = seq(80,370,20))+
+  labs(colour = "Data_source") +
+  xlab("Season days (centering around Jan 13)")+
+  ylab("Chill portion")+
+  ggtitle("Chill portions dependend on season days")+
+  theme_bw(base_size = 20)
+
+# The accumulated chill portions for the full season 
+ggplot(chill_comparison_max_season, aes(x=Chill_portions)) + 
+  geom_histogram(binwidth=1,aes(fill = factor(data_source))) +
+  theme_bw(base_size = 20) +
+  labs(fill = "Data source") +
+  xlab("Chill accumulation (Chill Portions)") +
+  ylab("Frequency")
+
+# Probability of reaching a specific number of chill portion
+chill_simulations<-chill_comparison_max_season[which(chill_comparison_max_season$data_source=="simulated"),]
+
+ggplot(chill_simulations, aes(x=Chill_portions)) +
+  stat_ecdf(geom = "step",lwd=1.5,col="blue") +
+  ylab("Cumulative probability") +
+  xlab("Chill accumulation (in Chill Portions)") +
+  theme_bw(base_size = 20)
+
+# Nice, seems like there are a lot of chilly days up there! :D
